344ba6a7e62955281ff6f00e190c1b1f507e0fe9
Phil Gibson <philg@cheatcodegames.net>
2023-03-15 11:00:58 +0000

    docs: document SquaredPlugin

--- src/main/kotlin/net/titanreborn/squared/core/plugin/SquaredPlugin.kt
+++ src/main/kotlin/net/titanreborn/squared/core/plugin/SquaredPlugin.kt
@@ -2,7 +2,17 @@ package net.titanreborn.squared.core.plugin
 
 import org.bukkit.plugin.java.JavaPlugin
 
+/**
+ * SquaredPlugin is the base class for all TitanSquared plugins. It provides the ability to access a variety of useful
+ * methods and utilities that are not provided by Bukkit or Paper, and integrates with other SquaredPlugins easily.
+ *
+ * @param name The name of the plugin. This is used to identify the plugin in the console and in the plugin.yml file.
+ */
 abstract class SquaredPlugin(name: String) : JavaPlugin() {
+    companion object {
+        private val plugins = mutableMapOf<Class<out SquaredPlugin>, SquaredPlugin>()
+    }
+
     override fun onEnable() {
         // Plugin startup logic
         enable()
@@ -13,6 +23,13 @@ abstract class SquaredPlugin(name: String) : JavaPlugin() {
         disable()
     }
 
+    /**
+     * Enables the plugin. This is called when the plugin is enabled.
+     */
     abstract fun enable()
+
+    /**
+     * Disables the plugin. This is called when the plugin is disabled.
+     */
     abstract fun disable()
 }
